2ee5415e63dd5d5ecc837d094349014f2c770a58
Wendy Garcia <wendy.fern84@gmail.com>
2019-09-13 13:50:31 -0700

    pseudocode for making the API request and saving data into variables

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+// Get click event and generate a function
+    // Get city from user's input #search-input store it in variable searchInput
+    // Generate the URL concatenating the searchInput where the "city" term goes
+
+    // Generate a search request from the API using ajax... then...
+        // Store the first 5 objects into an array of objects called events[];
+        
+    // Display with classes: result-name, result-date, result-venue, result-artist, result-description, and result-poster into the result-section
+
