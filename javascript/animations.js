2d7141dd7ea4a5ecf01c62448b60401345bf8b5b
Christin Boner <christinmclemons@gmail.com>
2019-09-18 08:35:53 -0700

    removing empty files i added but didnt need

--- javascript/animations.js
+++
@@ -1,7 +0,0 @@
-// $("#results-section").addEventListener("animationend", function () { 
-// var music = $("#music-results");
-// var sports = $("#sports-results");
-//     element.classList.add('animated', 'bounceOutLeft')
-//     element.classList.add('animated', 'bounceOutLeft')
-
-// })
