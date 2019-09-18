9319115083815fa2ae02ac86387a06bf101c21c7
Wendy Garcia <wendy.fern84@gmail.com>
2019-09-18 02:51:04 -0700

    info is appended directly from the object and not from the array, avoiding repetition of content

--- javascript/app.js
+++ javascript/app.js
@@ -9,6 +9,9 @@ var sports = [];
 $("#search-btn").on("click", function() {
   event.preventDefault();
 
+  $("#music-results").empty();
+  $("#sports-results").not("h5").not("p").empty();
+
   var searchCity = $("#search-input").val();
   var ticketMasterURL = "http://app.ticketmaster.com/discovery/v2/events.json?&size=200&city=" + searchCity + "&apikey=4tfR2LDAXpAcyulcEgARYYEfWZTLHCUQ";
 
@@ -19,37 +22,38 @@ $("#search-btn").on("click", function() {
   }).then(function(response) {
       console.log("searching for events in " + searchCity);
       console.log(response);
-      for (var i = 0;  i < response._embedded.events.length ; i++) {
-        
-        var newEvent = {
-          name : response._embedded.events[i].name,
-          venue : response._embedded.events[i]._embedded.venues[0].name,
-          info : response._embedded.events[i].info,
-          date : response._embedded.events[i].dates.start.localDate,
-          time : response._embedded.events[i].dates.start.localTime,
-          link : response._embedded.events[i].url,
-        };
-        //response._embedded.events[i].images    ratio: "3_2"
-        for (var j = 0 ; j < response._embedded.events[i].images.length ; j++ ) {
-          if (response._embedded.events[i].images[j].ratio === "3_2" && response._embedded.events[i].images[j].url.includes("ARTIST_PAGE")) {
-            newEvent.imageURL = response._embedded.events[i].images[j].url;
+
+      if(response._embedded.events.length === 0){
+        var noResultsMsg = $("<p>");
+        console.log("No Results to Display");
+        $("#result-section").append(noResultsMsg.text("No Results to Display"));
+      } else {
+
+        for (var i = 0;  i < response._embedded.events.length ; i++) {
+          
+          var newEvent = {
+            name : response._embedded.events[i].name,
+            venue : response._embedded.events[i]._embedded.venues[0].name,
+            info : response._embedded.events[i].info,
+            date : response._embedded.events[i].dates.start.localDate,
+            time : response._embedded.events[i].dates.start.localTime,
+            link : response._embedded.events[i].url,
+          };
+
+          for (var j = 0 ; j < response._embedded.events[i].images.length ; j++ ) {
+            if (response._embedded.events[i].images[j].ratio === "3_2" && response._embedded.events[i].images[j].url.includes("ARTIST_PAGE")) {
+              newEvent.imageURL = response._embedded.events[i].images[j].url;
+            }
+          }
+
+          if (response._embedded.events[i].classifications[0].segment.name === "Music") {
+            display(newEvent, "Music");
+          } else if (response._embedded.events[i].classifications[0].segment.name === "Sports") {
+            display(newEvent, "Sports");
           }
         }
-        if (response._embedded.events[i].classifications[0].segment.name === "Music") {
-          console.log("is music");
-          music.push(newEvent);
-          display(music, "Music");
-        } 
-        
-        if (response._embedded.events[i].classifications[0].segment.name === "Sports") {
-          console.log("is sports");
-          sports.push(newEvent);
-          display(sports, "Sports");
-          
 
-        }
       }
-      
     });
 
 });
@@ -57,30 +61,22 @@ $("#search-btn").on("click", function() {
 
 function display(events, type){
   console.log("-----");
-
-if(events.length === 0){
-  var noResultsMsg = $("<p>");
-  console.log("No Results to Display");
-  $("#result-section").append(noResultsMsg.text("No Results to Display"));
-
-} else {
-  for(var s=0; s < events.length; s++) {
     
     var eventImage = $("<img class='card-img-top'>");// add source and alttext
-    eventImage.attr("src",events[s].image);
+    eventImage.attr("src",events.imageURL);
     eventImage.attr("alt","image text");
     var eventName = $("<li class='list-group-item'>");
-    eventName.text(events[s].name);
+    eventName.text(events.name);
     var eventInfo = $("<li class='list-group-item'>");
-    eventInfo.text(events[s].info);
+    eventInfo.text(events.info);
     var eventDate= $("<li class='list-group-item'>");
-    eventDate.text(events[s].date);
+    eventDate.text(events.date);
     var eventTime= $("<li class='list-group-item'>");
-    eventTime.text(events[s].time);
+    eventTime.text(events.time);
     var eventVenue = $("<li class='list-group-item'>");
-    eventVenue.text(events[s].venue);
+    eventVenue.text(events.venue);
    var newUl= $("<ul list-group>");
-   newUl.append(eventName,eventInfo,eventDate,eventTime,eventVenue);
+   newUl.append(eventName,eventDate,eventTime,eventVenue,eventInfo);
     var newCard = $("<div class='card' style='width: 18rem;'>");
     //newCard.attr( );
     newCard.append(eventImage,newUl);
@@ -90,8 +86,5 @@ if(events.length === 0){
     } else if (type === "Sports") {
       $("#sports-results").append(newCard);
     }
-    
-  }
-}
 }
 
